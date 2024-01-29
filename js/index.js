4c348b5b37c9ee065c1650e2b62a94ac33707f3c
Bartas <bartas3@gmail.com>
2024-01-29 23:58:50 +0200

    meh

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 const am = document.getElementsByTagName("a");
 const as = Array.from(am);
-
+let active = false;
 function setActive(i) {
   as[i].classList.add("active");
 }
 function removeActive(i) {
-  as[i].classList.remove("active");
+  if (i != active) as[i].classList.remove("active");
 }
 function changeStatus(i) {
-  if (as[i].classList.contains("active")) removeActive(i);
-  else {
-    for (let x = 0; x < 4; x++) removeActive(x);
-    setActive(i);
+  active = i;
+  for (let x = 0; x < 4; x++) {
+    if (x == i) continue;
+    removeActive(x);
   }
+  setActive(i);
 }
 
 as.forEach((indicator, index) => {
-  indicator.addEventListener("onmouseover", function () {
+  indicator.addEventListener("pointerover", function () {
     setActive(index);
   });
-  indicator.addEventListener("onmouseout", function () {
+  indicator.addEventListener("pointerout", function () {
     removeActive(index);
   });
   indicator.addEventListener("click", function () {
